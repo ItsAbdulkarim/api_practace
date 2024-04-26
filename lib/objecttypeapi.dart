0c604ad82daa206254bb4b9a6c970995b2cd241b
ItsAbdulkarim <karimmuap@gmail.com>
2024-04-26 08:53:50 +0500

    with futurebuilder all object type

--- lib/objecttypeapi.dart
+++ lib/objecttypeapi.dart
@@ -29,98 +29,155 @@ class _ObjectTypeApiScreenState extends State<ObjectTypeApiScreen> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    getObjectTypeApi();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     var getdata = getObjectTypeApi();
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('objecttype api'),
-      ),
-      body: getObjectTypeApi() == null
-          ? Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                Text(model["page"].toString()),
-                Text(model["per_page"].toString()),
-                Text(model["total_pages"].toString()),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: model["data"].length,
-                    itemBuilder: (context, index) {
-                      var mylistt = model["data"][index];
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ListTile(
-                            leading: CircleAvatar(
-                              backgroundImage:
-                                  NetworkImage(mylistt["avatar"].toString()),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('objecttype api'),
+        ),
+        body: getObjectTypeApi() != null
+            ? Column(
+                children: [
+                  Text(model["page"].toString()),
+                  Text(model["per_page"].toString()),
+                  Text(model["total_pages"].toString()),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: model["data"].length,
+                      itemBuilder: (context, index) {
+                        var mylistt = model["data"][index];
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage(mylistt["avatar"].toString()),
+                              ),
+                              title: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(mylistt["id"].toString()),
+                                  Text(mylistt["email"].toString()),
+                                  Text(mylistt["first_name"].toString()),
+                                  Text(mylistt["last_name"].toString()),
+                                ],
+                              ),
                             ),
-                            title: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(mylistt["id"].toString()),
-                                Text(mylistt["email"].toString()),
-                                Text(mylistt["first_name"].toString()),
-                                Text(mylistt["last_name"].toString()),
-                              ],
-                            ),
-                          ),
 
-                          //also same we can access through futurebuilder
-                        ],
-                      );
-                    },
+                            //also same we can access through futurebuilder
+                          ],
+                        );
+                      },
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Expanded(
-                  child: FutureBuilder(
+                  SizedBox(
+                    height: 50,
+                  ),
+                  //
+                  Expanded(
+                    child: FutureBuilder(
+                      future: getObjectTypeApi(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          var mydataaa = snapshot.data!;
+                          return ListView.builder(
+                            itemCount: mydataaa["data"].length,
+                            itemBuilder: (context, index) {
+                              var futuredatalist = mydataaa["data"][index];
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  ListTile(
+                                    leading: CircleAvatar(
+                                      backgroundImage: NetworkImage(
+                                          futuredatalist["avatar"].toString()),
+                                    ),
+                                    title: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(futuredatalist["id"].toString()),
+                                        Text(
+                                            futuredatalist["email"].toString()),
+                                        Text(futuredatalist["first_name"]
+                                            .toString()),
+                                        Text(futuredatalist["last_name"]
+                                            .toString()),
+                                      ],
+                                    ),
+                                  ),
+
+                                  //also same we can access through futurebuilder
+                                ],
+                              );
+                            },
+                          );
+                        } else {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      },
+                    ),
+                  ),
+
+                  //we can access all through future builder
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FutureBuilder(
                     future: getObjectTypeApi(),
                     builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        var mydataaa = snapshot.data!;
-                        return ListView.builder(
-                          itemCount: mydataaa["data"].length,
-                          itemBuilder: (context, index) {
-                            var futuredatalist = mydataaa["data"][index];
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                ListTile(
-                                  leading: CircleAvatar(
-                                    backgroundImage: NetworkImage(
-                                        futuredatalist["avatar"].toString()),
-                                  ),
-                                  title: Column(
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('Error: ${snapshot.error}'));
+                      } else if (snapshot.hasData) {
+                        var snapshotdata = snapshot.data;
+
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(snapshotdata["page"].toString()),
+                            Text(
+                                'this is snap data${snapshotdata["per_page"].toString()}'),
+                            Text(model["total_pages"].toString()),
+                            Container(
+                              height: 400,
+                              child: ListView.builder(
+                                itemCount: snapshotdata['data'].length,
+                                itemBuilder: (context, index) {
+                                  var thislast = snapshotdata['data'][index];
+                                  return Column(
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
                                     children: [
-                                      Text(futuredatalist["id"].toString()),
-                                      Text(futuredatalist["email"].toString()),
-                                      Text(futuredatalist["first_name"]
-                                          .toString()),
-                                      Text(futuredatalist["last_name"]
-                                          .toString()),
+                                      Text(thislast["id"].toString()),
+                                      Text(thislast["email"].toString()),
+                                      Text(thislast["first_name"].toString()),
+                                      Text(thislast["last_name"].toString()),
+                                      Image.network(thislast["avatar"])
                                     ],
-                                  ),
-                                ),
-
-                                //also same we can access through futurebuilder
-                              ],
-                            );
-                          },
+                                  );
+                                },
+                              ),
+                            )
+                          ],
                         );
                       } else {
-                        return Center(child: CircularProgressIndicator());
+                        return Text('no record found');
                       }
                     },
-                  ),
-                )
-              ],
-            ),
-    );
+                  )
+                ],
+              )
+            : Center(child: CircularProgressIndicator()));
   }
 }
